e0b55998524e30d38d659b34a3bd1162b7a970d5
rjnpage <rankajkumar58@gmail.com>
2026-03-31 21:52:52 +0530

    Add MainActivity

---
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.myapp;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("APK Build Success");
+        setContentView(tv);
+    }
+}
